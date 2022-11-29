abb39961be759fc01c4bd07634953c56f7f4c4fb
Nguyen-1210 <ghetcongai137@gmail.com>
2022-11-29 16:23:57 +0700

    [admin] branch sung đột

--- admin/index.php
+++ admin/index.php
@@ -213,15 +213,7 @@ if (isset($_GET['act'])) {
             include "./donhang/list.php";
             break;
 
-<<<<<<< HEAD
-        case 'list_charts':
-            $listthongke = chart();
-            include "./thongke/list.php";
-            break;
-        
 
-=======
->>>>>>> chart
         case 'chart':
             $listthongke = chart();
             include "./thongke/chart.php";
